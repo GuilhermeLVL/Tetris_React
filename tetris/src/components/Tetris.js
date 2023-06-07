7790a35b4f7ee8b31c70ebe0583bfdfe1a5f99d6
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-06-07 14:14:16 -0300

    a

--- tetris/src/components/Tetris.js
+++ tetris/src/components/Tetris.js
@@ -4,6 +4,16 @@ import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
 
+const Tetris = () => {
 
+    return(
+        <div>
+            <Stage></Stage>
+            <aside>
+                <Display text="score"></Display>
+            </aside>
+        </div>
+    )
+}
 
 export default Tetris;

2fdc9a12a1a9dba5abc4a55289ed605bd882f6aa
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-06-08 16:19:47 -0300

    a

--- tetris/src/components/Stage.js
+++ tetris/src/components/Stage.js
@@ -6,7 +6,7 @@ import Cell from "./Cell";
 
 const Stage = ({ stage }) => (
 
-<StyledStage>
+<StyledStage width={stage[0].length} height={stage.length}>
     {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} ></Cell>))}
 </StyledStage>
 );

c71093d531918dc98888f5440f8db2feab1428c9
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-06-08 15:33:44 -0300

    f

--- tetris/src/components/Cell.js
+++ tetris/src/components/Cell.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../tetromicos";
 
 const Cell =({type}) => (
 
-<div>cell</div>
+<StyledCell type={'type'} color={TETROMINOS[type].color}>cell</StyledCell>
 
 
 );

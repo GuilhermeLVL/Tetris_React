a300f63dd3cdde272f077ccb3a4d05652cc6ef95
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-06-14 17:18:59 -0300

    Update gameHelpers.js

--- tetris/src/gameHelpers.js
+++ tetris/src/gameHelpers.js
@@ -10,6 +10,20 @@ export const checkCollision = (player, stage, {x: moveX, y:moveY }) => {
     for(let y = 0; y < player.tetrominio.length; y += 1){
         for(let x = 0; x < player.tetrominio[0].length; x += 1){
 
+            if ( player.tetrominio[y][x] !== 0){
+                
+                if(
+              //(y)
+              stage[y+ player.pos.y + moveY] ||
+
+              !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+
+              stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
+
+              ) {
+                return true
+              }
+            }
         }
     }
 }

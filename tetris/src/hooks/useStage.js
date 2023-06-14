a9e7ef29112f27e711267e9fc1262ac6ba1e09f3
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-06-14 18:05:09 -0300

    W

--- tetris/src/hooks/useStage.js
+++ tetris/src/hooks/useStage.js
@@ -2,15 +2,14 @@ import { useState, useEffect } from "react";
 import { createStage } from "../gameHelpers";
 
 export const useStage = (player, resetPlayer) => {
+    const [stage, setStage] = useState(createStage());
 
-    const [ stage, setStage] = useState(createStage())
-
-    useEffect(() =>{
+    useEffect(() => {
         const updateStage = prevStage => {
-
-            const newStage = prevStage.map(row =>
-                row.map(cell =>(cell[1] ==='clear' ? [0,'clear'] : cell)),
-                );
+          // First flush the stage
+          const newStage = prevStage.map(row =>
+            row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+          );
 
                 player.tetrominio.forEach((row, y) =>{
                   row.forEach((value, x) => {
@@ -19,13 +18,14 @@ export const useStage = (player, resetPlayer) => {
                         newStage[y + player.pos.y][x + player.pos.x] = [
                             value,
                             `${player.collided ? 'merged' : 'clear'}`,
-                        ]
+                          ];
                     }
                   } )  
                 })
 
                 if(player.collided){
                     resetPlayer()
+                    
                 }
 
             return newStage;    
